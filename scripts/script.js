5b090a9d9384d1e9d8da1835c54877e8780ee5e5
Jorge Reyes <rjorge99@hotmail.com>
2024-02-12 14:07:59 -0600

    Se agrega manejo de links

--- scripts/script.js
+++ scripts/script.js
@@ -8,6 +8,20 @@ const api = (function () {
         imageElement.src = isCurremtHamburguer ? 'images/arrow-icon.svg' : 'images/hamburguer-icon.svg';
     }
 
+    // Click behaviour
+    (function addClickEventToNavLinks() {
+        const links = document.getElementsByClassName('nav-link');
+        for (let i = 0; i < links.length; i++) {
+            links[i].addEventListener('click', function ({ target }) {
+                document.querySelectorAll('.active').forEach((e) => e.classList.remove('active'));
+
+                const href = target.attributes['href'].value.replace('#', '');
+                const linksToActivate = document.getElementsByClassName(`lnk${href}`);
+                for (let y = 0; y < linksToActivate.length; y++) linksToActivate[y].classList.add('active');
+            });
+        }
+    })();
+
     return {
         toggleSideMenu
     };
