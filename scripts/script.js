947a40f32af392e1bc9fbff41d556abd0447c9b4
Jorge Reyes <rjorge99@hotmail.com>
2024-02-12 16:22:51 -0600

    Se termina parte del cascaron

--- scripts/script.js
+++ scripts/script.js
@@ -13,32 +13,62 @@ const api = (function () {
         const links = document.getElementsByClassName('nav-link');
         for (let i = 0; i < links.length; i++) {
             links[i].addEventListener('click', function ({ target }) {
-                document.querySelectorAll('.active').forEach((e) => e.classList.remove('active'));
-
-                const href = target.attributes['href'].value.replace('#', '');
-                const linksToActivate = document.getElementsByClassName(`lnk${href}`);
-                for (let y = 0; y < linksToActivate.length; y++) linksToActivate[y].classList.add('active');
+                removeActiveLinks();
+                setActiveLink(target.attributes['href'].value.replace('#', ''));
             });
         }
     })();
 
+    function setActiveLink(href) {
+        const linksToActivate = document.getElementsByClassName(`lnk${href}`);
+        for (let y = 0; y < linksToActivate.length; y++) linksToActivate[y].classList.add('active');
+    }
+
+    function removeActiveLinks() {
+        document.querySelectorAll('.active').forEach((e) => e.classList.remove('active'));
+    }
+
     document.addEventListener('scroll', function () {
+        removeActiveLinks();
+
         const principalSection = document.querySelector('#principales');
+        const generalesSection = document.querySelector('#generales');
+        const ventasSection = document.querySelector('#ventas');
+        const almacenSection = document.querySelector('#almacen');
+        const contabilidadSection = document.querySelector('#contabilidad');
+        const disposicionesSection = document.querySelector('#disposiciones');
         const demostracionSection = document.querySelector('#demostracion');
+
         let scrollPosition = window.scrollY || document.documentElement.scrollTop;
 
-        if (principalSection) {
-            if (scrollPosition >= principalSection.offsetTop && scrollPosition < principalSection.offsetTop + principalSection.offsetHeight)
-                document.getElementsByClassName('navbar')[0].classList.remove('navbar--dark');
-            else document.getElementsByClassName('navbar')[0].classList.add('navbar--dark');
-        }
-        if (demostracionSection) {
-            if (scrollPosition >= demostracionSection.offsetTop && scrollPosition < demostracionSection.offsetTop + demostracionSection.offsetHeight)
-                document.getElementsByClassName('navbar')[0].classList.add('navbar--blue');
-            else document.getElementsByClassName('navbar')[0].classList.remove('navbar--blue');
-        }
-    });
+        // Principal
+        if (scrollPosition >= principalSection.offsetTop && scrollPosition < principalSection.offsetTop + principalSection.offsetHeight) {
+            document.getElementsByClassName('navbar')[0].classList.remove('navbar--dark');
+            setActiveLink('principales');
+        } else document.getElementsByClassName('navbar')[0].classList.add('navbar--dark');
 
+        // Generales
+        if (scrollPosition >= generalesSection.offsetTop && scrollPosition < generalesSection.offsetTop + generalesSection.offsetHeight)
+            setActiveLink('generales');
+
+        //Ventas
+        if (scrollPosition >= ventasSection.offsetTop && scrollPosition < ventasSection.offsetTop + ventasSection.offsetHeight) setActiveLink('ventas');
+
+        //Almacen
+        if (scrollPosition >= almacenSection.offsetTop && scrollPosition < almacenSection.offsetTop + almacenSection.offsetHeight) setActiveLink('almacen');
+
+        //Contabilidad
+        if (scrollPosition >= contabilidadSection.offsetTop && scrollPosition < contabilidadSection.offsetTop + contabilidadSection.offsetHeight)
+            setActiveLink('contabilidad');
+
+        // Disposiciones
+        if (scrollPosition >= disposicionesSection.offsetTop && scrollPosition < disposicionesSection.offsetTop + disposicionesSection.offsetHeight)
+            if (scrollPosition >= demostracionSection.offsetTop && scrollPosition < demostracionSection.offsetTop + demostracionSection.offsetHeight) {
+                // Demostracion
+                document.getElementsByClassName('navbar')[0].classList.add('navbar--blue');
+                setActiveLink('demostracion');
+            } else document.getElementsByClassName('navbar')[0].classList.remove('navbar--blue');
+    });
     return {
         toggleSideMenu
     };
